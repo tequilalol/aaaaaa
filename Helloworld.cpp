68ccc6c9406c90031c07c037079df4183232cf56
Roman <horde_mag@mail.ru>
2013-02-26 11:45:41 +0400

    gdfg
    fgdg

---
+++ Helloworld.cpp
@@ -0,0 +1,14 @@
+// Helloworld.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <conio.h>
+#include <iostream>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout<<"Hello world";
+	return 0;
+}
+
